a4d7f29a6c7adb975762a5face09a180025013dc
Tommi Pernila <tommi.pernila@iki.fi>
2015-10-25 17:25:14 +0200

    version 0.2

--- Syyua.sh
+++ Syyua.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 #tommi 24.10.2015
-#Syyua.sh version 0,1
+#Syyua.sh version 0,2
 
 echo 'Syyua implementation'
+
+
+if [ -e "/usr/sbin/pkg" ]
+then
+	echo'found pkg'
+else
+	pkg
+fi
+
+if [ -e "/var/db/portsnap/INDEX" ]
+then
+	echo'ports extracted'
+else
+	portsnap fetch extract
+fi
+
+if [ -e "/usr/local/sbin/portmaster" ]
+then
+	echo'portmaster installed'
+else
+	pkg install portmaster
+fi
+
+
 if [ -e "/etc/defaults/pcbsd" ]
 then
 	echo 'PCBSD'
@@ -40,3 +64,4 @@ else
 fi
 
 echo 'Done'
+
