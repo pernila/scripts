7737c07aea05391d528d91b0afdeae973e8d32ff
Tommi Pernila <tommi.pernila@iki.fi>
2016-03-16 15:18:37 +0200

    more modules to kldload shell

--- load_i915kms.sh
+++ load_i915kms.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-echo 'This script loads the kernel module i915kms'
+echo 'This script loads the kernel module i915kms and some acpi modules'
 
 
 #dmesg from boot
 
+kldload acpi_video &
+kldload acpi_ibm &
 kldload i915kms &
 
