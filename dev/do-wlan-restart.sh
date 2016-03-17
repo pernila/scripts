9c2cf20813aa6db906bd5906e1e070646b8e3698
Tommi Pernila <tommi.pernila@iki.fi>
2016-03-17 15:04:31 +0200

    wlan restart fixes

--- dev/do-wlan-restart.sh
+++ dev/do-wlan-restart.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 #
+
 echo 'wlan restart with netif restart and restart dhclient for wlan0'
-
 service -v netif restart 
-
 sleep 1
+echo ''
 
+echo 'ssid:'
+ifconfig wlan0 ssid EWA@GUEST
+echo ''
+
+echo 'kill dhclient:'
 killall dhclient
-
+echo ''
+sleep 1
+echo 'dhclient:'
+dhclient wlan0
+echo ''
 sleep 1
 
-dhclient wlan0
-
-sleep 2
-
+echo 'network info:'
 basic-network-info.sh
-
 echo ''
 echo 'restart script end'
-
